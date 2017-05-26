4af932be70c89d4aa2c5822c6b5d7b5263f78c13
Patrik Nordwall <patrik.nordwall@gmail.com>
2017-05-26 15:11:29 +0200

    Timers

---
+++ src/main/java/blog/typed/javadsl/BuncherApp.java
@@ -0,0 +1,73 @@
+package blog.typed.javadsl;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+import java.io.IOException;
+import scala.concurrent.duration.Duration;
+
+import akka.typed.ActorRef;
+import akka.typed.ActorSystem;
+import akka.typed.Behavior;
+import akka.typed.javadsl.Actor;
+
+public class BuncherApp {
+
+  public abstract static class BuncherDestination {
+    private BuncherDestination() {
+    }
+
+    public static class Info implements Buncher.Msg {
+      public final String s;
+
+      public Info(String s) {
+        this.s = s;
+      }
+
+      @Override
+      public String toString() {
+        return "Info(" + s + ")";
+      }
+    }
+
+    public static Behavior<Buncher.Batch> behavior() {
+      return Actor.immutable(Buncher.Batch.class)
+        .onMessage(Buncher.Batch.class, (ctx, batch) -> {
+          System.out.println("Got batch of " + batch.getMessages().size() + " messages: " + batch.getMessages());
+          return Actor.same();
+        })
+        .build();
+    }
+
+  }
+
+  public static void main(String[] args) throws IOException {
+    Behavior<Void> root = Actor.deferred(ctx -> {
+
+      ActorRef<Buncher.Batch> destination =
+          ctx.spawn(BuncherDestination.behavior(), "destination");
+
+      ActorRef<Buncher.Msg> buncher =
+        ctx.spawn(Buncher.behavior(destination, Duration.create(3, SECONDS), 10), "buncher");
+
+      for (int n = 1; n <= 15; n++) {
+        buncher.tell(new BuncherDestination.Info(String.valueOf(n)));
+      }
+
+      ctx.schedule(Duration.create(1, SECONDS), buncher, new BuncherDestination.Info("16"));
+      ctx.schedule(Duration.create(2, SECONDS), buncher, new BuncherDestination.Info("17"));
+
+      ctx.schedule(Duration.create(4, SECONDS), buncher, new BuncherDestination.Info("18"));
+
+      return Actor.empty();
+    });
+    ActorSystem<Void> system = ActorSystem.create("Sys", root);
+    try {
+      System.out.println("Press ENTER to exit the system");
+      System.in.read();
+    } finally {
+      system.terminate();
+    }
+  }
+
+}
+
---
+++ src/main/scala/blog/typed/scaladsl/Buncher.scala
@@ -0,0 +1,90 @@
+package blog.typed.scaladsl
+
+import scala.concurrent.duration._
+import scala.io.StdIn
+
+import akka.typed.ActorRef
+import akka.typed.ActorSystem
+import akka.typed.Behavior
+import akka.typed.scaladsl.Actor
+import akka.typed.scaladsl.TimerScheduler
+
+object Buncher {
+  trait Msg
+  final case class Batch(messages: Vector[Msg])
+
+  private case object TimerKey
+  private case object Timeout extends Msg
+
+  def behavior(target: ActorRef[Batch], after: FiniteDuration, maxSize: Int): Behavior[Msg] =
+    Actor.withTimers(timers => idle(timers, target, after, maxSize))
+
+  private def idle(timers: TimerScheduler[Msg], target: ActorRef[Batch],
+    after: FiniteDuration, maxSize: Int): Behavior[Msg] = {
+    Actor.immutable[Msg] { (ctx, msg) =>
+      timers.startSingleTimer(TimerKey, Timeout, after)
+      active(Vector(msg), timers, target, after, maxSize)
+    }
+  }
+
+  private def active(buffer: Vector[Msg], timers: TimerScheduler[Msg],
+    target: ActorRef[Batch], after: FiniteDuration, maxSize: Int): Behavior[Msg] = {
+    Actor.immutable[Msg] { (ctx, msg) =>
+      msg match {
+        case Timeout =>
+          target ! Batch(buffer)
+          idle(timers, target, after, maxSize)
+        case msg =>
+          val newBuffer = buffer :+ msg
+          if (newBuffer.size == maxSize) {
+            timers.cancel(TimerKey)
+            target ! Batch(newBuffer)
+            idle(timers, target, after, maxSize)
+          } else
+            active(newBuffer, timers, target, after, maxSize)
+      }
+    }
+  }
+}
+
+object BuncherDestination {
+
+  final case class Info(s: String) extends Buncher.Msg
+
+  val behavior: Behavior[Buncher.Batch] =
+    Actor.immutable[Buncher.Batch] { (ctx, batch) =>
+      println(s"Got batch of ${batch.messages.size} messages: ${batch.messages.mkString(", ")} ")
+      Actor.same
+    }
+}
+
+object BuncherApp {
+
+  def main(args: Array[String]): Unit = {
+    val root = Actor.deferred[Nothing] { ctx =>
+      val destination = ctx.spawn(BuncherDestination.behavior, "destination")
+      val buncher = ctx.spawn(
+        Buncher.behavior(destination, 3.seconds, 10),
+        "buncher")
+
+      (1 to 15).foreach { n =>
+        buncher ! BuncherDestination.Info(n.toString)
+      }
+
+      ctx.schedule(1.second, buncher, BuncherDestination.Info("16"))
+      ctx.schedule(2.seconds, buncher, BuncherDestination.Info("17"))
+
+      ctx.schedule(4.seconds, buncher, BuncherDestination.Info("18"))
+
+      Actor.empty
+    }
+    val system = ActorSystem[Nothing]("Sys", root)
+    try {
+      // Exit the system after ENTER is pressed
+      StdIn.readLine()
+    } finally {
+      system.terminate()
+    }
+  }
+
+}
